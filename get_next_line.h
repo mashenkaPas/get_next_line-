db23599278db334393dd97ba53c1eee371307320
mashenkaPas <42279868+mashenkaPas@users.noreply.github.com>
2018-11-27 19:40:52 -0800

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpas <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/26 18:00:17 by mpas              #+#    #+#             */
+/*   Updated: 2018/11/27 15:40:36 by mpas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+#endif
